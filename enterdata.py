de30c98a7b3a1699cfcbc4752c34c2d98750a465
g-w1 <58830309+g-w1@users.noreply.github.com>
2019-12-31 17:26:38 -0500

    Update enterdata.py

--- enterdata.py
+++ enterdata.py
@@ -4,4 +4,35 @@ pygame.init()
 class Screen:
   def __init__(self):
     self.pixels = [pixel(x,y) for x in range(25) for y in range(25)]
- 
+	def draw(self):
+		for pixel in self.pixels:
+			pixel.draw()
+	def update(self):
+		self.loop = True
+		while self.loop:
+			self.draw()
+			if pygame.key.get_pressed()[pygame.K_s]:
+				self.loop = False
+				self.iden = [1,0]
+			elif pygame.key.get_pressed()[pygame.K_s]:
+				self.loop = False
+				self.iden = [0,1]
+		return (np.asarray(np.reshape([x.value for x in self.pixels], (625,1)),self.iden)
+class Pixel:
+	def __init__(self,x,y):
+		self.value = 0.0
+		self.rect = (x,y,1,1)
+	def draw(self):
+		if pygame.mouse.get_pressed()[0] and pygame.Rect(self.rect).collidepoint(pygame.mouse.get_pos()):
+			self.value+=20
+		pygame.draw.rect(win,(255*self.value,255*self.value,255*self.value),self.rect)
+if __name__  = "__main__":
+	data = np.load("data.npy")
+	exit = False
+	while not(exit):
+		for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+					exit = True
+		data.append(Screen().update())
+		np.save("data",data)
+	np.save("data",data)
