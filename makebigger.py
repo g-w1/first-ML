442d0dfbcbb274e2c0344ba9d433159758ec721b
g-w1 <58830309+g-w1@users.noreply.github.com>
2020-01-01 08:08:41 -0500

    Update makebigger.py

--- makebigger.py
+++ makebigger.py
@@ -1,21 +1,22 @@
-#originally made by miachel nielson, i made it multipy the data by nine instead of 5
+#originally made by miachel nielson
+# i am also using .npy files instead of .gz files
 import os.path
 import random
 
 # Third-party libraries
 import numpy as np
 
-print("Expanding the MNIST training set")
+print("Expanding the training set")
 
-if os.path.exists("../data/mnist_expanded.pkl.gz"):
+if os.path.exists("data_expanded.npy"):
     print("The expanded training set already exists.  Exiting.")
 else:
-    f = gzip.open("../data/mnist.pkl.gz", 'rb')
-    training_data, validation_data, test_data = cPickle.load(f)
-    f.close()
+    training_data = np.load("data.npy")
     expanded_training_pairs = []
     j = 0 # counter
-    for x, y in zip(training_data[0], training_data[1]):
+    for example in training_data:
+        x = example[0]
+        y = example[1]
         expanded_training_pairs.append((x, y))
         image = np.reshape(x, (-1, 28))
         j += 1
@@ -36,6 +37,4 @@ else:
     random.shuffle(expanded_training_pairs)
     expanded_training_data = [list(d) for d in zip(*expanded_training_pairs)]
     print("Saving expanded data. This may take a few minutes.")
-    f = gzip.open("../data/mnist_expanded.pkl.gz", "w")
-    cPickle.dump((expanded_training_data, validation_data, test_data), f)
-    f.close()
+    np.save("data_expanded",expanded_training_data)
