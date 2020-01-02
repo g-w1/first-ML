ef3a96dfd7ce91d09bf1711de7bfd10640ef1466
g-w1 <58830309+g-w1@users.noreply.github.com>
2020-01-01 21:26:27 -0500

    end of day

--- enterdata.py
+++ enterdata.py
@@ -1,29 +1,46 @@
 import numpy as np
 import pygame
+skip = False
 pygame.init()
 class Screen:
-  def __init__(self,scale):
-    self.scale = scale
-		self.pixels = [pixel(x,y,self.scale) for x in range(25) for y in range(25)]
+	def __init__(self,scale):
+		self.scale = scale
+		self.pixels = []
+		for y in range(25):
+			for x in range(25):
+				self.pixels.append(Pixel(x,y,self.scale))
 	def draw(self):
 		for pixel in self.pixels:
 			pixel.draw()
 	def update(self):
 		self.loop = True
+		global skip
+		skip = False
 		while self.loop:
 			self.draw()
 			if pygame.key.get_pressed()[pygame.K_s]:
 				self.loop = False
 				self.iden = [[1],[0]]
-			elif pygame.key.get_pressed()[pygame.K_s]:
+				print(np.asarray(np.reshape([x.value for x in self.pixels], (625,1))),self.iden)
+				return (np.asarray(np.reshape([x.value for x in self.pixels], (625,1))),np.asarray(self.iden))
+			if pygame.key.get_pressed()[pygame.K_f]:
 				self.loop = False
 				self.iden = [[0],[1]]
-		return (np.asarray(np.reshape([x.value for x in self.pixels], (625,1)),np.asarray(self.iden))
+				print(np.asarray(np.reshape([x.value for x in self.pixels], (625,1))))
+				return (np.asarray(np.reshape([x.value for x in self.pixels], (625,1))),np.asarray(self.iden))
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					self.loop = False
+					skip = True
+		print("skipped")
 	def update_test(self):
 		self.loop = True
 		while self.loop:
 			self.draw()
 			if pygame.key.get_pressed()[pygame.K_SPACE]:
+				self.loop = False
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
 				self.loop = False
 		return numpy.asarray([[x.value] for x in self.pixels])
 	
@@ -31,20 +48,25 @@ class Pixel:
 	def __init__(self,x,y,scale):
 		self.scale = scale
 		self.value = 0.0
-		self.rect = (x,y,self.scale,self.scale)
+		self.x = x*scale
+		self.y = y*scale
+		self.rect = (self.x,self.y,self.scale,self.scale)
+		self.sq = scale
 	def draw(self):
-		if pygame.mouse.get_pressed()[0] and pygame.Rect(self.rect).collidepoint(pygame.mouse.get_pos()):
-			self.value+=20
-		pygame.draw.rect(win,(255*self.value,255*self.value,255*self.value),self.rect)
-if __name__  = "__main__":
-	scale = 4
+		global win
+		if self.clicked():
+			self.value=1
+		pygame.draw.rect(win,(round((255*self.value)),round(255*self.value),round(255*self.value)),(self.x,self.y,self.sq,self.sq))
+	def clicked(self):
+		if pygame.mouse.get_pressed()[0] == True and pygame.Rect((self.x,self.y,self.sq,self.sq)).collidepoint(pygame.mouse.get_pos()) == True:
+			return True
+		else:
+			return False
+if __name__  == "__main__":
+	scale = 25
 	win = pygame.display.set_mode((25*scale,25*scale))
-	data = np.load("data.npy")
-	exit = False
-	while not(exit):
-		for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-		exit = True
-	data.append(Screen(scale).update())
-	np.save("data",data)
-	np.save("data",data)
+	win.fill((5,5,5))
+	if not(skip):	
+		data = np.load("data.npy")
+		np.append(data,Screen(scale).update())
+		np.save("data",data)
--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import enterdata
 screen  = Screen()
 network = Network([625,15,2],inputweights,inputbiases)
 output = network.feedforward(screen.update_test())
-if np.argmax(output) = output[1]:
+if np.argmax(output) = output[1][0]:
   print(frowney face)
 else:
   print(smiely face)
