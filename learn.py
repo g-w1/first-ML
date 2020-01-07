842c2541bc9b4532b18d296497ed97f7c3b7ab96
g-w1 <58830309+g-w1@users.noreply.github.com>
2020-01-07 10:17:21 -0500

    Update learn.py
    documenting

--- learn.py
+++ learn.py
@@ -5,33 +5,28 @@ import pickle
 import os
 import numpy as np
 class Population:
+	#class to manage networks
 	def __init__(self,n,layers):
 		self.number = n
 		self.genepool = []
 		self.pop = []
 		for x in range(n):
 			self.pop.append(Network(layers))
+		#makes n networks with layers layers
 	def addgenepool(self):
 		global training_data
 		for network in self.pop:
 			for x in round(network.fitness_numcor(training_data)):
-				self.genepool.append(network.mutate(.04))
+				self.genepool.append(network.mutate(.04,.1))
+		#adds each network to the genepool its finess times
 	def createnewpop(self):
 		self.pop = []
 		for x in range(self.number):
 			self.pop.append(self.genepool.pop(random.randint(0,len(self.genepool)-1)))
+		#takes the genepool and selects a random element to add to the pop n times
 class Network:
     def __init__(self, sizes, weights = None, biases = None):
-        """The list ``sizes`` contains the number of neurons in the
-        respective layers of the network.  For example, if the list
-        was [2, 3, 1] then it would be a three-layer network, with the
-        first layer containing 2 neurons, the second layer 3 neurons,
-        and the third layer 1 neuron.  The biases and weights for the
-        network are initialized randomly, using a Gaussian
-        distribution with mean 0, and variance 1.  Note that the first
-        layer is assumed to be an input layer, and by convention we
-        won't set any biases for those neurons, since biases are only
-        ever used in computing the outputs from later layers."""
+		#creates a network of size layers with random weights and biases unless weighs and biases are specified
         self.num_layers = len(sizes)
         self.sizes = sizes
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
@@ -42,32 +37,36 @@ class Network:
             self.weights = weights
 
     def feedforward(self, a):
-        """Return the output of the network if ``a`` is input."""
+        #Return the output of the network if a is input
         for b, w in zip(self.biases, self.weights):
             a = sigmoid(np.dot(w, a)+b)
         return a
-
     def fitness_numcor(self,training_data):
+		#a fitness function for a network that is calculated by how many it gets correct
         for img in test_data:
             test = self.feedforward(img[0])
-
             if test[0][0]>test[1][0] and img[1][0][0]>img[1][1][0]:
                 corr+=1
             if test[0][0]<test[1][0] and img[1][0][0]<img[1][1][0]:
                 corr+=1
-        if corr/len(training_data)>70:
+        if 100(corr/len(training_data))>70:
             print("working")
         return 100 *(corr/len(training_data))
     def fitness_cost(self,training_data):
+		#a fitness function for a network that is calculated inversely to the average cost
 		costtotal = []
         for img in test_data:
             test = self.feedforward(img[0])
             cost = np.mean(np.absolute(img[1]-test))
             costtotal.append(cost)
-        return 100*(np.mean(costtotal))
-    def mutate(self,mutation_rate):
-
+		z = 100*(1/np.mean(costtotal))
+		if z>70:
+			print("working")
+        return z
+    def mutate(self,mutation_rate,changerate):
+		#changes mutation_rate elements changerate*1,-1
 		def inputrandom(n,mutationrate,changerate):
+			#returns a mutated number given an input
 			r = random.uniform(0, 1)
 			if r<mutationrate:
 				a = random.randint(0,1)
@@ -77,17 +76,25 @@ class Network:
 		for i in self.weights:
 			for a in i:
 				for b in a:
-					inputrandom(b,.mutationrate,.1)
+					inputrandom(b,.mutationrate,changerate)
 		for i in self.biases:
 			for a in i:
 				for b in a:
-					inputrandom(b,.mutationrate,.1)
+					inputrandom(b,.mutationrate,changerate)
 def sigmoid(z):
-    """The sigmoid function."""
+    #The sigmoid function
     return 1.0/(1.0+np.exp(-z))
-    #return np.maximum(0,z)
+#the topology of each neuron
 layers = [625,10,2]
 if __name__ == "__main__":
     f = open("data/data_expanded.data","rb")
     training_data = pickle.load(f)
     f.close()
+	#create 1000 networks
+	networks = Population(1000,layers)
+	exit = False
+	while not(exit):
+		#main loop
+		networks.addgenepool()
+		networks.createnewpop()
+	
